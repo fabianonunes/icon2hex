bbc2d8573d8b3d3a796a4397b0c5ae3b0c3ce392
Fabiano Nunes <fabiano@nunes.me>
2012-12-19 14:27:28 -0200

    test workspace

---
+++ src/test/java/tc/fab/icon2hex/Icon2HexTest.java
@@ -0,0 +1,14 @@
+package tc.fab.icon2hex;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class Icon2HexTest extends TestCase {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
